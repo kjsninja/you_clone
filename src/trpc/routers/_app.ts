8265299667fa3065c98ae080939544cb4f942fd3
kjsninja <kendrick004@gmail.com>
2025-04-17 11:20:56 +0800

    move trpc to specific folder

---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,7 @@
+import { createTRPCRouter, protectedProcedure } from '@/trpc/init';
+import { categoriesRouter } from '@/modules/categories/server/procedures';
+export const appRouter = createTRPCRouter({
+  categories: categoriesRouter
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
